833cfdf3de4f477280bb62ad13eff6b0e6c1e37f
Awesxme <manasvi.d007@gmail.com>
2026-04-25 18:07:59 +0000

    thread pool

--- async/src/hard/thread_pool.rs
+++ async/src/hard/thread_pool.rs
@@ -9,6 +9,7 @@
     cargo test --test thread_pool_test
 */
 
+use std::sync::mpsc::Sender;
 use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 
@@ -21,14 +22,54 @@ type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     pub fn new(size: usize) -> Self {
-        todo!()
+        let (tx, rx) = mpsc::channel::<Job>();
+
+        let mut workers: Vec<Worker> = Vec::with_capacity(size);
+        
+        let receiver = Arc::new(Mutex::new(rx));
+
+        for id in 0..size {
+            let thread_receiver = Arc::clone(&receiver);
+            
+            let thread = thread::spawn(move || {
+                loop {
+                    // 1. Lock the mutex to get access to the receiver.
+                    // 2. Try to receive a job.
+                    let result = thread_receiver.lock().expect("Mutex poisoned").recv();
+
+                    match result {
+                        Ok(job) => {
+                            // Execute the closure!
+                            job();
+                        }
+                        Err(_) => {
+                            // If recv() returns an error, the sender was dropped.
+                            // This is our signal to shut down the thread.
+                            break;
+                        }
+                    }
+                }
+            });
+
+            workers.push(Worker {
+                id,
+                thread: Some(thread),
+            });
+        }
+
+        ThreadPool {
+            workers,
+            sender: tx,
+        }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
-        todo!()
+        let job = Box::new(f);
+        
+        self.sender.send(job).unwrap()
     }
 }
 
