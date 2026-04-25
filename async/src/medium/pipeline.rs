2c11999facb801e09ca80c51595685f0a401d69f
Awesxme <manasvi.d007@gmail.com>
2026-04-25 14:55:29 +0000

    pipeline

--- async/src/medium/pipeline.rs
+++ async/src/medium/pipeline.rs
@@ -15,5 +15,33 @@ use std::sync::mpsc;
 use std::thread;
 
 pub fn data_pipeline() -> i32 {
-    todo!()
+    let (tx, rx) = mpsc::channel();
+    let (tx1, rx1) = mpsc::channel();
+
+    let handle1 = thread::spawn(move || {
+      for i in 1..=5 {
+        tx.send(i).unwrap();
+      }
+    });
+
+    let handle2 = thread::spawn(move || {
+      for x in rx {
+        tx1.send(x*x).unwrap();
+      }
+    });
+
+    let handle3 = thread::spawn(move || {
+      let mut local_sum = 0;
+      for x in rx1 {
+        local_sum += x;
+      }
+      local_sum
+    });
+
+    handle1.join().unwrap();
+    handle2.join().unwrap();
+
+    let sum = handle3.join().unwrap();
+
+    sum
 }
