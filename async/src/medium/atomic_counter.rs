2ffb1fa072f295076dfa23cbaedbb55cbda9ac2d
Awesxme <manasvi.d007@gmail.com>
2026-04-26 10:27:25 +0000

    AtomicI32

--- async/src/medium/atomic_counter.rs
+++ async/src/medium/atomic_counter.rs
@@ -14,5 +14,28 @@ use std::sync::Arc;
 use std::thread;
 
 pub fn atomic_counter() -> i32 {
-    todo!()
+  let counter = Arc::new(AtomicI32::new(0));
+  let mut handles = vec![];
+
+  for _ in 1..=10 {
+    let count = Arc::clone(&counter);
+    let handle = thread::spawn(move ||{
+      for _ in 1..=100 {
+        count.fetch_add(1, Ordering::SeqCst);
+      }
+    });
+
+    handles.push(handle);
+  }
+
+  for handle in handles {
+    handle.join().unwrap();
+  }
+
+  // mutex guard i sdropped here with the semicolon
+  // this can't be the tail expression, as in that case,
+  // the counter is cropped while the mutex guard is still alive!
+  let res = counter.load(Ordering::SeqCst);
+
+  res
 }
