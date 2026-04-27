2ba0543685722bead21990ce9fa1bc695bff485b
Awesxme <manasvi.d007@gmail.com>
2026-04-27 15:37:14 +0000

    broadcast

--- async/src/medium/broadcast_message.rs
+++ async/src/medium/broadcast_message.rs
@@ -13,5 +13,31 @@
 use tokio::sync::broadcast;
 
 pub async fn broadcast_demo() -> i32 {
-    todo!()
+    let (tx, _rx) = broadcast::channel::<i32>
+    (16);
+
+    let mut handles = vec![];
+
+    for i in 0..3 {
+      let mut rx = tx.subscribe();
+
+      let handle = tokio::spawn(async move {
+        match rx.recv().await {
+          Ok(val) => {
+            val
+          }
+          Err(_) => 0
+        }
+      });
+      handles.push(handle);
+    }
+
+    let _ = tx.send(10);
+
+    let mut total_sum = 0;
+    for handle in handles {
+        total_sum += handle.await.unwrap_or(0);
+    }
+
+    total_sum
 }
