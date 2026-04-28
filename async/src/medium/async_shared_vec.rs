8a4dc1e231e535ea13dee6dec888ed97987dfb88
Awesxme <manasvi.d007@gmail.com>
2026-04-28 04:38:11 +0000

    async shared vec

--- async/src/medium/async_shared_vec.rs
+++ async/src/medium/async_shared_vec.rs
@@ -13,5 +13,27 @@ use tokio::sync::Mutex;
 use std::sync::Arc;
 
 pub async fn async_shared_vec() -> usize {
-    todo!()
+    let shared_vec = Arc::new(Mutex::new(Vec::new()));
+
+    let mut handles = vec![];
+
+    for _ in 0..5 {
+      let vec_clone = Arc::clone(&shared_vec);
+
+      let handle = tokio::spawn(async move {
+        for i in 0..10 {
+          let mut v = vec_clone.lock().await;
+          v.push(i);
+        }
+      });
+      handles.push(handle);
+    }
+
+    for handle in handles {
+      let _ = handle.await;
+    }
+
+    let final_vec = shared_vec.lock().await;
+
+    final_vec.len()
 }
