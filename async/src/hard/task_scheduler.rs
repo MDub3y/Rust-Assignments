85fe07a35c362f98fbd23e21f946272c50119189
Awesxme <manasvi.d007@gmail.com>
2026-04-28 13:21:58 +0000

    task scheduler

--- async/src/hard/task_scheduler.rs
+++ async/src/hard/task_scheduler.rs
@@ -18,17 +18,49 @@ pub struct TaskScheduler {
 
 impl TaskScheduler {
     pub fn new() -> Self {
-        todo!()
+        Self {
+            tasks: Arc::new(Mutex::new(Vec::new()))
+        }
     }
 
     pub fn schedule<F>(&self, delay: Duration, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
-        todo!()
+        let run_at = Instant::now() + delay;
+        let mut tasks = self.tasks.lock().unwrap();
+        
+        tasks.push((run_at, Box::new(f)));
+        
+        tasks.sort_by(|a, b| b.0.cmp(&a.0));
     }
 
     pub fn start(&self) {
-        todo!() // Spawn a background task to process the queue
+        let tasks_clone = Arc::clone(&self.tasks);
+
+        tokio::spawn(async move {
+            loop {
+                let task_to_run = {
+                    let mut tasks = tasks_clone.lock().unwrap();
+                    
+                    if let Some((time, _)) = tasks.last() {
+                        if Instant::now() >= *time {
+                            let (_, f) = tasks.pop().unwrap();
+                            Some(f)
+                        } else {
+                            None
+                        }
+                    } else {
+                        None
+                    }
+                };
+
+                if let Some(f) = task_to_run {
+                    f();
+                } else {
+                    sleep(Duration::from_millis(10)).await;
+                }
+            }
+        });
     }
 }
