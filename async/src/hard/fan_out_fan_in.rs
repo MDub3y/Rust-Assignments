051f577f3f9dbf46aaaa41a1111124dcc4591681
Awesxme <manasvi.d007@gmail.com>
2026-04-28 13:04:14 +0000

    fan-ou fan-in

--- async/src/hard/fan_out_fan_in.rs
+++ async/src/hard/fan_out_fan_in.rs
@@ -10,5 +10,27 @@
 */
 
 pub async fn fan_out_fan_in(v: Vec<i32>) -> i32 {
-    todo!()
+  let mut handles = vec![];
+
+  for x in v {
+      let handle = tokio::spawn(async move {
+          x * x
+      });
+      handles.push(handle);
+  }
+
+  let mut total_sum = 0;
+
+  for handle in handles {
+      match handle.await {
+          Ok(squared_val) => {
+              total_sum += squared_val;
+          }
+          Err(e) => {
+              eprintln!("Task panicked: {:?}", e);
+          }
+      }
+  }
+
+  total_sum
 }
