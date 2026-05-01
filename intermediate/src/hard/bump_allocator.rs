eea549bf21cb926e0c0bd46e19b32911f8fed5b8
Awesxme <manasvi.d007@gmail.com>
2026-05-01 10:30:41 +0000

    bump allocator

--- intermediate/src/hard/bump_allocator.rs
+++ intermediate/src/hard/bump_allocator.rs
@@ -16,10 +16,23 @@ pub struct BumpAllocator {
 
 impl BumpAllocator {
     pub fn new() -> Self {
-        todo!()
+        Self {
+            buffer: [0; 1024],
+            cursor: 0,
+        }
     }
 
     pub fn alloc(&mut self, size: usize) -> Result<&mut [u8], String> {
-        todo!()
+        let end = self.cursor + size;
+
+        if end > self.buffer.len() {
+            return Err("Out of memory".to_string());
+        }
+
+        let allocation = &mut self.buffer[self.cursor..end];
+
+        self.cursor = end;
+
+        Ok(allocation)
     }
 }
